ee5f87243f851750a7603e25a42e80f3f241f369
Dalibor Lipovac <dalibor.lipovac@digitalinsurance.io>
2020-05-20 15:33:38 +0200

    Decipher extended to handle symbols

--- scramble.js
+++ scramble.js
@@ -1,24 +1,30 @@
 const str = 'nzzvy://skkz.muumrk.ius/mmv-wojh-ten';
 
 function unscramble(code, cycles) {
-  const destructuredCode = code.match(/[a-zA-Z]/g);
   const actualCycles = cycles % 52;
-
-  return destructuredCode.map(char => {
+  
+  return str.split('').map(char => {
     var asciiCodeOfChar = char.charCodeAt();
     // first letter decimal code in ascii table
     // depending on being uppercase or lowercase
     var asciiStart;
+    var diff;
 
     // if character is uppercase letter
     if (asciiCodeOfChar >= 65 && asciiCodeOfChar <= 90) {
       asciiStart = 65;
+      diff = (asciiCodeOfChar - asciiStart + actualCycles) % 26;
     }
     // if character is lowercase letter
     else if (asciiCodeOfChar >= 97 && asciiCodeOfChar <= 122) {
       asciiStart = 97;
+      diff = (asciiCodeOfChar - asciiStart + actualCycles) % 26;
     }
-    const diff = (asciiCodeOfChar - asciiStart + actualCycles) % 26;
+    // all other cases (symbols and everything)
+    else {
+      asciiStart = char.charCodeAt();
+      diff = 0;
+    }
     return String.fromCharCode(diff + asciiStart);
   }).join('');
 }
